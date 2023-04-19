ced53f792a77d787d465891d3bce6684163fbdd8
Robert Kwoll <robert.kwoll@itsilesia.com>
2023-04-19 14:39:15 +0200

    Add components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import "./fonts/Gumela Regular.otf";
+import Navbar from "./components/navbar/Navbar";
+import Intro from "./components/navbar/intro/Intro";
+import Services from "./components/services/Services";
 
 function App() {
-  return <div className="App">I am here with new settings</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Intro />
+      <Services />
+    </div>
+  );
 }
 
 export default App;
