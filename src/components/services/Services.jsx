2e6eeb24958dd9092715cef7867290f3c747be3a
Robert Kwoll <robert.kwoll@itsilesia.com>
2023-04-19 15:23:18 +0200

    Create Services

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Services.css";
+import heartemoji from "../../img/heartemoji.png";
+import glasses from "../../img/glasses.png";
+import humble from "../../img/humble.png";
+import Card from "../card/Card";
+import Resume from "./Resume.pdf";
+
+const Services = () => {
+  return (
+    <div className="services">
+      {/* Left side */}
+      <div className="awesome">
+        <span>My Awesome</span>
+        <span>services</span>
+        <spane>
+          Lorem ipsum <br /> Lorem ipsum
+        </spane>
+        <a href={Resume} download>
+          {/* Podmienić potem na CV */}
+          <button className="button s-button"> Download CV</button>
+        </a>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+
+      {/* Right side */}
+      <div className="cards">
+        <div style={{ left: "14rem" }}>
+          <Card
+            emoji={heartemoji}
+            heading={"Design"}
+            detail="Figma, Sketch, Photoshop, Adobe, Adobe XD"
+          />
+        </div>
+
+        <div style={{ top: "12rem", left: "-4rem" }}>
+          <Card
+            emoji={glasses}
+            heading={"Developer"}
+            detail="Html, Css, Javascript, React, React Native"
+          />
+        </div>
+
+        <div style={{ top: "19rem", left: "12rem" }}>
+          <Card
+            emoji={humble}
+            heading={"UI/UX"}
+            detail="Lorem ipsum dummy text are"
+          />
+        </div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
