78093a441fded55ee5a5c323f60365fc16ed9608
Robert Kwoll <robert.kwoll@itsilesia.com>
2023-04-24 15:11:14 +0200

    Create component toogle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import "./fonts/Gumela Regular.otf";
 import Navbar from "./components/navbar/Navbar";
-import Intro from "./components/navbar/intro/Intro";
+import Intro from "./components/intro/Intro";
 import Services from "./components/services/Services";
 import Experience from "./components/experience/Experience";
 import Works from "./components/works/Works";
@@ -9,10 +9,20 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
+import { themeContext } from "./Context";
+import { useContext } from "react";
 
 function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}
+    >
       <Navbar />
       <Intro />
       <Services />
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./Navbar.css";
+import Toogle from "../toogle/Toogle";
 
 const Navbar = () => {
   return (
     <div className="n-wrapper">
       <div className="n-left">
         <div className="n-name">Robert</div>
-        <span>toogle</span>
+        <Toogle />
       </div>
       <div className="n-right">
         <div className="n-list">
-          <ul style={{ listStyleType: 'none' }}>
+          <ul style={{ listStyleType: "none" }}>
             <li>Home</li>
             <li>Services</li>
             <li>Experience</li>
